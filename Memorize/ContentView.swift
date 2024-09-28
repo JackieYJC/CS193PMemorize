97ea626bd8922c6cddfad22787b21f1badbb371a
Jackie Yang <jackie19970410@gmail.com>
2024-09-28 18:00:46 -0400

    CargView isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View { // not OOP at all. This struct behaves like a View
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
         }
+        .foregroundColor(.cyan)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false // has to have a value (initialize)
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 25.0) // no way to both stroke and fill a shape (!)
+                    .strokeBorder(lineWidth: 3) // order matters
+                Text("🐶")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
