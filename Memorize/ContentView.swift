ba869631e65dac683f8649b72079bfae01e6ea96
Jackie Yang <jackie19970410@gmail.com>
2024-09-28 19:36:31 -0400

    Finished RT1 Assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,17 +11,62 @@
 import SwiftUI
 
 struct ContentView: View { // Not OOP at all. This struct behaves like a View
-    let emojis: [String] = ["🤖", "👹", "👾", "😈", "🤠"] // Or Array<String>
+    let emojis: [String] = ["🤖", "👹", "👾", "😈", "🤠", "👺", "🎃", "💩"] // Or Array<String>
     
-    var body: some View {
-        HStack { // Trailing closure syntax
+    @State // Not frequently used in real-code, good for demos
+    var cardCount: Int = 4
+    
+    var body: some View { // Computed View
+        VStack { // This is a @ViewBuilder
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+        .padding() // This is for VStack itself, not recursively applied
+    }
+    
+    var cards: some View { // Implicit return
+        
+        // Trailing closure syntax
+        // Making things in it as small as possible
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             // 0..<5, 0...5 from 0 up to and including 5; ForEach View
-            ForEach(emojis.indices, id: \.self) { index in // Arguments to closures
+            ForEach(0..<cardCount, id: \.self) { index in // Arguments to closures
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .foregroundColor(.cyan)
-        .padding()
+        .foregroundColor(.red)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+    }
+    
+    // by is the external param name, offset is the internal param name
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
 
@@ -33,19 +78,21 @@ struct CardView: View { // Views are immutable (!!); you need backend to change
     var body: some View {
         // Trailing closure syntax; @ViewBuilder allows limited content, can't do for-loop
         ZStack(alignment: .center) {
-            let base = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/) // Local variable; type inference
+            let base = RoundedRectangle(cornerRadius: 15) // Local variable; type inference
             
-            if isFaceUp {
+            Group { // Like a ForEach of 1, allows applying properties to group of Views
                 base
                     .fill(.white)
                 base // No way to both stroke and fill a shape (!)
                     .strokeBorder(lineWidth: 3) // Order matters (!)
                 Text(content)
                     .font(.largeTitle)
-            } else {
-                base
-                    .fill() // Fill or stroke. Fill is default
             }
+//            .opacity(isFaceUp ? 1 : 0)
+            // Opacity is good way to toggle visibility without impacting spacing
+            
+            // Fill it with foreground color if isFaceUp is false
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }.onTapGesture { // View Modifier; normal code, no longer a @ViewBuilder
             // print("tapped") // Print to console
             isFaceUp.toggle()
